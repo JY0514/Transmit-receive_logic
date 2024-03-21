e613d2a418cb660dfdb06caa2512c0e09f1770e3
JY0514 <jy287454@gmail.com>
2024-03-21 17:26:55 +0900

    group_all 테이블에 총 운행시간 오류 해결 중_0321

--- main.py
+++ main.py
@@ -66,7 +66,6 @@ def recep():
     conn = dbconnect()
     cursor = conn.cursor()
     cursor.execute("SELECT group_id,rider_id, start_time, end_time, r_date, u_date FROM logic.r_info")
-
     sql2 = " SELECT rider_id, group_id,MIN(start_time) AS earliest_start_time, MAX(end_time) AS latest_end_time FROM  logic.r_info GROUP BY rider_id;"
     cursor.execute(sql2)
     result2 = cursor.fetchall()
@@ -79,92 +78,68 @@ def recep():
         end_time = row[3]
 
         c_operating = end_time - start_time
-
-    sql3 = f"""
-        # INSERT INTO logic.group_info (c_operating, group_id, rider_id, start_time, end_time, r_date, u_date)
-        # SELECT '{c_operating}', '{group_id}', '{rider_id}', '{start_time}', '{end_time}', '{r_date}', '{r_date}'
-        # WHERE NOT EXISTS (
-        #     SELECT 1 FROM logic.group_info
-        #     WHERE group_id = '{group_id}' AND rider_id = '{rider_id}'
-        # );
-        
+        sql3 = f"""
         INSERT INTO logic.group_info (c_operating, group_id, rider_id, start_time, end_time, r_date, u_date)
-SELECT '{c_operating}', '{group_id}', '{rider_id}', '{start_time}',
-       CASE 
-           WHEN EXISTS (
-               SELECT 1 FROM r_info WHERE group_id = '{group_id}' AND end_time IS NULL
-           ) THEN NULL 
-           ELSE '{end_time}' 
-       END AS end_time,
-       '{r_date}', '{r_date}'
-WHERE NOT EXISTS (
-    SELECT 1 FROM logic.group_info WHERE group_id = '{group_id}' AND rider_id = '{rider_id}'
-);
-
+        SELECT '{c_operating}', '{group_id}', '{rider_id}', '{start_time}', '{end_time}', '{r_date}', '{r_date}'
+        WHERE NOT EXISTS (
+        SELECT 1 FROM logic.group_info
+        WHERE group_id = '{group_id}' AND rider_id = '{rider_id}'
+        );
         """
+        cursor.execute(sql3)
+        conn.commit()
 
-    cursor.execute(sql3)
-    conn.commit()
+        sql_check = ("SELECT COUNT(*)"
+                     "FROM logic.group_info GROUP BY group_id, rider_id HAVING COUNT(*) > 1;")
+        cursor.execute(sql_check)
 
-    sql_check = ("SELECT COUNT(*)"
-                 "FROM logic.group_info GROUP BY group_id, rider_id HAVING COUNT(*) > 1;")
-    cursor.execute(sql_check)
-    result3 = cursor.fetchall()
-    print(result3)
+        sql_update = (f"UPDATE logic.group_info gi SET gi.start_time = (SELECT MAX(ri.start_time)"
+                      f" FROM r_info ri WHERE ri.rider_id = gi.rider_id),"
+                      f" gi.end_time = (SELECT MAX(ri.end_time) FROM r_info ri WHERE ri.rider_id = gi.rider_id), gi.u_date = NOW()"
+                      f" WHERE gi.group_id = '{group_id}'")
+        cursor.execute(sql_update)
+        conn.commit()
+        c_operating = end_time - start_time
+        days = c_operating.days
+        seconds = c_operating.seconds
+        minutes_from_days = days * 24 * 60
+        hours_from_seconds = seconds // 3600
+        minutes_from_seconds = (seconds % 3600) // 60
+        total_minutes = minutes_from_days + hours_from_seconds * 60 + minutes_from_seconds
+        d_amount = total_minutes * 20
 
-    sql_update = (f"UPDATE logic.group_info gi SET gi.start_time = (SELECT MAX(ri.start_time)"
-                  f" FROM r_info ri WHERE ri.rider_id = gi.rider_id),"
-                  f" gi.end_time = (SELECT MAX(ri.end_time) FROM r_info ri WHERE ri.rider_id = gi.rider_id), gi.u_date = NOW()"
-                  f" WHERE gi.group_id = '{group_id}'")
-    cursor.execute(sql_update)
-    conn.commit()
-    c_operating = end_time - start_time
-    print(c_operating)
-    days = c_operating.days
-    seconds = c_operating.seconds
-    minutes_from_days = days * 24 * 60
-    hours_from_seconds = seconds // 3600
-    minutes_from_seconds = (seconds % 3600) // 60
-    total_minutes = minutes_from_days + hours_from_seconds * 60 + minutes_from_seconds
-    d_amount = total_minutes * 20
+        update_query = f"UPDATE  logic.group_info  SET d_amount = '{d_amount}' WHERE rider_id = '{rider_id}'"
+        cursor.execute(update_query)
+        conn.commit()
 
-    update_query = f"UPDATE  logic.group_info  SET d_amount = '{d_amount}' WHERE rider_id = '{rider_id}'"
-    cursor.execute(update_query)
-    conn.commit()
+        sql = f"SELECT rider_id, COUNT(oper_id) AS start_count , r_date FROM logic.r_info GROUP BY rider_id;"
+        cursor.execute(sql)
+        conn.commit()
+        result_d = cursor.fetchall()
+
+        for row in result_d:
+            rider_id = row[0]
+            start_count = row[1]
+            r_dates = row[2]
+
+            cursor.execute("SELECT COUNT(*) FROM logic.group_all WHERE rider_id = %s", (rider_id,))
+            result_count = cursor.fetchone()[0]
+
+            sql12 = f"SELECT rider_id, SEC_TO_TIME(SUM(TIMESTAMPDIFF(SECOND, start_time, end_time))) AS total_driving_time FROM logic.r_info ri GROUP BY rider_id;"
+            cursor.execute(sql12)
+            sql12result = cursor.fetchall()
+
+            for row in sql12result:
+                print("넘어왔음")
+                total_driving_time = row[1]
+                formatted_time = str(total_driving_time)
+
+            if result_count == 0:
+                sql222 = "INSERT INTO logic.group_all (rider_id, start_count, r_date,u_date, driving_time) VALUES (%s, %s, %s,NOW(), %s)"
+                cursor.execute(sql222, (rider_id, start_count, r_dates, formatted_time))
 
 
-def end():
-    conn = dbconnect()
-    cursor = conn.cursor()
-    cursor.execute("SELECT group_id,rider_id, start_time, end_time, r_date, u_date FROM logic.group_info")
-
+        conn.commit()
 
 send()
 recep()
-
-# 수신 종료 로직
-
-# 라이더에 대해 end_ount _+1을 한다.
-
-# 라이더의 해당하는 call 의 종료시간과 운행 상태를 업데이트를 계속 한다.
-
-
-# 만약 동일하다면 end_count의 값이 1이 +되고  그와 동시에 운행상태가 업데이트된다.
-
-# 그리고 # 업데이트를 하고나서 end_count, start_count가 group_all테이블에 있는것과 동일한지 체크한다.
-
-# 해당 기사에 대해 gruoup_count + 1을 한다.
-
-# groupid를 채번을 하고
-
-# 채번한 그룹아이디가 다건이기때문에 call roawData에 ㅇ업데이트하여 추가한다.
-
-#  그리고 그룹 아이디ㅡㄹ 전부 조회한다.
-
-#  시작 시간 산출 후 가장 늦은 시간을 변수에 담고
-
-# 시작시간/종료시간/운행시간 으로 보험료를 산출한다.
-
-# groupall_info에 최종 데이터를 저장한다.
-
-#     그룹 수신에서 사용할 코드
