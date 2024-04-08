0e1faef5d0392d52d75143e2ff261b13405947a4
JY0514 <jy287454@gmail.com>
2024-04-08 17:56:06 +0900

    그룹카운트 수정중_0408

--- main_reception.py
+++ main_reception.py
@@ -3,6 +3,8 @@ import pymysql
 from datetime import datetime
 import string
 import random
+from collections import defaultdict
+
 
 app = Flask(__name__)
 
@@ -74,6 +76,7 @@ def start():
     }
     return jsonify(response)
 
+
 @app.route("/reception/end", methods=['POST'])  # 운행종료
 def end():
     params = request.get_json(silent=True)
@@ -117,188 +120,198 @@ def end():
     conn.commit()
 
     sql_end_time = f"""
-                SELECT  end_time 
-                FROM logic.r_info where end_time like '{end_date_str}%'
-                GROUP BY rider_id;
+                    SELECT  end_time 
+                    FROM logic.r_info where end_time like '{end_date_str}%'
+                    GROUP BY rider_id;
     """
     cursor.execute(sql_end_time)
     conn.commit()
     result = cursor.fetchone()
     # 여기까진 맞고
 
+    # data format
     result_test = result[0].strptime(end_time, "%Y-%m-%d %H:%M:%S")
     result_tests = result_test.strftime("%Y-%m-%d")
 
     #  start_count 와 end_count 동일한지 확인 동일하면 1로 나옴
+    # check = f"""
+    #             SELECT CASE
+    #             WHEN start_count = end_count THEN 1
+    #             ELSE 0
+    #             END AS 일치여부
+    #             FROM logic.group_all
+    #             WHERE rider_id = '{rider_id}' and driving_time like '{result_tests}%';
+    #                       """   #여기서 8개가 나와서 문제임 7개가 나와야하는건데
+    #
     check = f"""
-                SELECT CASE
+           SELECT DISTINCT rider_id, 
+           CASE
                 WHEN start_count = end_count THEN 1
                 ELSE 0
-                END AS 일치여부
-                FROM logic.group_all
-                WHERE rider_id = '{rider_id}' and driving_time like '{result_tests}%';  
-                          """
+           END AS 일치여부 , driving_time
+           FROM logic.group_all
+           WHERE rider_id = '{rider_id}' OR driving_time LIKE '{result_tests}%'
+           GROUP BY rider_id, start_count, end_count;
+    """
+
     cursor.execute(check)
     conn.commit()
     result_check = cursor.fetchall()
-    if str(result_check) == '((1,),)':  # 일치한 경우 1
+    print(result_check)
+    date_dict = defaultdict(list)
 
-        # group_all 테이블 group_count + 1
-        sql_g_count = f"""
-              update logic.group_all set group_count = group_count + 1 where rider_id = '{rider_id}'
-              """
-        cursor.execute(sql_g_count)
-        conn.commit()
-
-        # 랜덤으로 그룹아이디 생성
-        letters_set = string.ascii_letters
-        random_list = random.sample(letters_set, 3)
-        result_id = ''.join(random_list)
-        group_id = "IDG_" + result_id
-        # 생성한 그룹아이디 업데이트
-        sqls = f""" UPDATE r_info SET group_id = '{group_id}' WHERE group_id is null AND rider_id = '{rider_id}' """
-        cursor.execute(sqls)
-        conn.commit()
-
-        # group_id 여부 확인
-        sql_check_group = f"""
-            SELECT COUNT(*)
-            FROM logic.r_info
-            WHERE group_id = '{group_id}';
-        """
-        cursor.execute(sql_check_group)
-        conn.commit()
-        result_group_check = cursor.fetchone()[0]
-        if result_group_check != 0:  # 그룹아이디 존재하는것만 group_info생성
-
-            # group_info 생성
-            sql_info = f"""
-                    INSERT IGNORE INTO logic.group_info (group_id, rider_id)
-                    SELECT %s,%s
-                    WHERE NOT EXISTS (
-                    SELECT 1 FROM logic.group_info
-                    WHERE group_id = '{group_id}' ); """
-
-            cursor.execute(sql_info, (group_id, rider_id))
+    for row in result_check:
+        datetime = datetime.strptime(row[2], "%Y-%m-%d %H:%M:%S.%f")
+        date_key = datetime.date()
+        date_dict[date_key].append(row)
+        for date_key, rows in date_dict.items():
+            print(f"날짜: {date_key}")
+            for row in rows:
+        if str(row[1]) == '1':  # 일치한 경우 1
+            # group_all 테이블 group_count + 1, 중복 업데이트 방지
+            sql_g_count = f"""
+                  UPDATE logic.group_all 
+                  SET group_count = group_count + 1 
+                  WHERE rider_id = '{row[0]}';
+                  """
+            cursor.execute(sql_g_count)
             conn.commit()
 
-            # start_time 제일 이른 시간
-            sql_g_time = f"""       select
-                                    DATE_FORMAT(MIN(start_time), '%Y-%m-%d %T')
-                                    from logic.r_info
-                                    where group_id = '{group_id}'
-                                    """
-            cursor.execute(sql_g_time)
+            # 랜덤으로 그룹아이디 생성
+            letters_set = string.ascii_letters
+            random_list = random.sample(letters_set, 3)
+            result_id = ''.join(random_list)
+            group_id = "IDG_" + result_id
+            # 생성한 그룹아이디 업데이트
+            sqls = f""" UPDATE r_info SET group_id = '{group_id}' WHERE group_id is null AND rider_id = '{rider_id}' """
+            cursor.execute(sqls)
             conn.commit()
-            result = cursor.fetchone()
-            first_start_time = result[0]
-
-            if first_start_time is not None:
-                start_time_f = datetime.strptime(first_start_time, "%Y-%m-%d %H:%M:%S")
-                # c_operating 분으로 변경 및 보험료 구하는 계산
-                c_operating = end_time_s - start_time_f
-                minutes = c_operating.total_seconds() / 60
-                oper_m = int(minutes)  # 건마다 들어가는 운행시간
-                total_minutes = c_operating.days * 1440 + c_operating.seconds / 60
-                d_amount = total_minutes * 19  # 건마다 들어가는 보험료
-
-                # 여기서 업데이트해야 밑에 쿼리문이 돌아감
-                sql_info_update = f"""
-                                  UPDATE logic.group_info
-                                  SET start_time = '{first_start_time}', end_time = '{end_time}',
-                                  d_amount = '{d_amount}', c_operating = '{oper_m}' , u_date = NOW() , r_date = NOW()
-                                  WHERE group_id = '{group_id}'
-                                   """
-                cursor.execute(sql_info_update)
+            # group_id 여부 확인
+            sql_check_group = f"""
+                SELECT COUNT(*)
+                FROM logic.r_info
+                WHERE group_id = '{group_id}';
+            """
+            cursor.execute(sql_check_group)
+            conn.commit()
+            result_group_check = cursor.fetchone()[0]
+            if result_group_check != 0:  # 그룹아이디 존재하는것만 group_info생성
+                # group_info 생성
+                sql_info = f"""
+                        INSERT IGNORE INTO logic.group_info (group_id, rider_id)
+                        SELECT %s,%s
+                        WHERE NOT EXISTS (
+                        SELECT 1 FROM logic.group_info
+                        WHERE group_id = '{group_id}' ); """
+                cursor.execute(sql_info, (group_id, rider_id))
                 conn.commit()
-
-                # 라이더마다 운행시간이 총 300분인지 확인
-                sql_rider_oper = f"""
-                             SELECT DATE(start_time), SUM(c_operating), SUM(d_amount)
-                FROM logic.group_info
-                WHERE rider_id = '{rider_id}'
-                GROUP BY DATE(start_time)
-                ORDER BY DATE(start_time);"""
-
-                cursor.execute(sql_rider_oper)
-                conn.commit()
-                result = cursor.fetchall()
-
-                for row in result:
-                    date, oper, d = row
-                    oper_time = oper - oper_m     #현재 추가되는 운행 제외하고 기존에 있던 시간
-                    d_amount_now = d - d_amount  # 현재 추가되는 운행 제외하고 기존에 있던 보험료
-                    time = oper - oper_time  # 현재 업데이트하려는 시간
-
-                    for date in row:
-                        if oper_time > 300:  # 현재 누적 운행 시간이 300이 넘을 때
-
-                            print("현재 누적 운행 시간이 300분 초과")
-                            sql_date_check = f"""
-                                            SELECT DATE(start_time), DATE(end_time)
-                                            FROM logic.group_info
-                                            WHERE group_id = '{group_id}'
+                # start_time 제일 이른 시간
+                sql_g_time = f"""       select
+                                        DATE_FORMAT(MIN(start_time), '%Y-%m-%d %T')
+                                        from logic.r_info
+                                        where group_id = '{group_id}'
                                         """
-                            cursor.execute(sql_date_check)
-                            conn.commit()
-                            date_result = cursor.fetchall()
-                            if len(date_result) != 1 or date_result[0][0] != date_result[0][1]:  # start_time과 end_time 날짜가 다른 경우
-                                d_amounts = 0
-                                sql_info_update = f"""
-                                                 UPDATE logic.group_info
-                                                 SET start_time = '{first_start_time}', end_time = '{end_time_s}',
-                                                 d_amount = '{d_amounts}', c_operating = '{oper_m}' , u_date = NOW() , r_date = NOW()
-                                                 WHERE group_id = '{group_id}'
-                                             """
-                                cursor.execute(sql_info_update)
+                cursor.execute(sql_g_time)
+                conn.commit()
+                result = cursor.fetchone()
+                first_start_time = result[0]
+                if first_start_time is not None:
+                    start_time_f = datetime.strptime(first_start_time, "%Y-%m-%d %H:%M:%S")
+                    # c_operating 분으로 변경 및 보험료 구하는 계산
+                    c_operating = end_time_s - start_time_f
+                    minutes = c_operating.total_seconds() / 60
+                    oper_m = int(minutes)  # 건마다 들어가는 운행시간
+                    total_minutes = c_operating.days * 1440 + c_operating.seconds / 60
+                    d_amount = total_minutes * 19  # 건마다 들어가는 보험료
+                    # 여기서 업데이트해야 밑에 쿼리문이 돌아감
+                    sql_info_update = f"""
+                                      UPDATE logic.group_info
+                                      SET start_time = '{first_start_time}', end_time = '{end_time}',
+                                      d_amount = '{d_amount}', c_operating = '{oper_m}' , u_date = NOW() , r_date = NOW()
+                                      WHERE group_id = '{group_id}'
+                                       """
+                    cursor.execute(sql_info_update)
+                    conn.commit()
+                    # 라이더마다 운행시간이 총 300분인지 확인
+                    sql_rider_oper = f"""
+                                 SELECT DATE(start_time), SUM(c_operating), SUM(d_amount)
+                    FROM logic.group_info
+                    WHERE rider_id = '{rider_id}'
+                    GROUP BY DATE(start_time)
+                    ORDER BY DATE(start_time);"""
+                    cursor.execute(sql_rider_oper)
+                    conn.commit()
+                    result = cursor.fetchall()
+                    for row in result:
+                        date, oper, d = row
+                        oper_time = oper - oper_m  # 현재 추가되는 운행 제외하고 기존에 있던 시간
+                        d_amount_now = d - d_amount  # 현재 추가되는 운행 제외하고 기존에 있던 보험료
+                        time = oper - oper_time  # 현재 업데이트하려는 시간
+                        for date in row:
+                            if oper_time > 300:  # 현재 누적 운행 시간이 300이 넘을 때
+                                print("현재 누적 운행 시간이 300분 초과")
+                                sql_date_check = f"""
+                                                SELECT DATE(start_time), DATE(end_time)
+                                                FROM logic.group_info
+                                                WHERE group_id = '{group_id}'
+                                            """
+                                cursor.execute(sql_date_check)
                                 conn.commit()
-
-
-                            else:  # start_time과 end_time 날짜가 같은 경우
-                                d_amounts = 0
+                                date_result = cursor.fetchall()
+                                if len(date_result) != 1 or date_result[0][0] != date_result[0][
+                                    1]:  # start_time과 end_time 날짜가 다른 경우
+                                    d_amounts = 0
+                                    sql_info_update = f"""
+                                                     UPDATE logic.group_info
+                                                     SET start_time = '{first_start_time}', end_time = '{end_time_s}',
+                                                     d_amount = '{d_amounts}', c_operating = '{oper_m}' , u_date = NOW() , r_date = NOW()
+                                                     WHERE group_id = '{group_id}'
+                                                 """
+                                    cursor.execute(sql_info_update)
+                                    conn.commit()
+                                else:  # start_time과 end_time 날짜가 같은 경우
+                                    d_amounts = 0
+                                    sql_info_update = f"""
+                                                       UPDATE logic.group_info
+                                                                         SET start_time = '{first_start_time}', end_time = '{end_time_s}',
+                                                                         d_amount = '{d_amounts}', c_operating = '{oper_m}' , u_date = NOW() , r_date = NOW()
+                                                                         WHERE group_id = '{group_id}' 
+                                                   """
+                                    cursor.execute(sql_info_update)
+                                    conn.commit()
+                            elif time + oper_time > 300:  # 지금 추가되는 시간이 300이 넘는지
+                                # 새로 추가되는 시간이 더해져야함
+                                print("지금까지의 운행 시간에 현재 운행 시간이 더해져서 300분이 초과")
+                                d_amounts = 6700 - d_amount_now
                                 sql_info_update = f"""
-                                                   UPDATE logic.group_info
-                                                                     SET start_time = '{first_start_time}', end_time = '{end_time_s}',
-                                                                     d_amount = '{d_amounts}', c_operating = '{oper_m}' , u_date = NOW() , r_date = NOW()
-                                                                     WHERE group_id = '{group_id}' 
-                                               """
-                                cursor.execute(sql_info_update)
-                                conn.commit()
-
-                        elif time + oper_time > 300:  # 지금 추가되는 시간이 300이 넘는지
-                            # 새로 추가되는 시간이 더해져야함
-                            print("지금까지의 운행 시간에 현재 운행 시간이 더해져서 300분이 초과")
-                            d_amounts = 6700 - d_amount_now
-                            sql_info_update = f"""
-                                            UPDATE logic.group_info
-                                            SET start_time = '{first_start_time}', end_time = '{end_time_s}',
-                                            d_amount = '{d_amounts}', c_operating = '{oper_m}' , u_date = NOW() , r_date = NOW()
-                                            WHERE group_id = '{group_id}'
-                                             """
-                            cursor.execute(sql_info_update)
-                            conn.commit()
-                        elif time < 300:  # 지금 추가되는 시간이 300이 넘는지
-                            sql_info_update = f"""
                                                 UPDATE logic.group_info
                                                 SET start_time = '{first_start_time}', end_time = '{end_time_s}',
-                                                d_amount = '{d_amount}', c_operating = '{oper_m}' , u_date = NOW() , r_date = NOW()
+                                                d_amount = '{d_amounts}', c_operating = '{oper_m}' , u_date = NOW() , r_date = NOW()
                                                 WHERE group_id = '{group_id}'
                                                  """
-                            cursor.execute(sql_info_update)
-                            conn.commit()
-                        else:  # 운행시간이 300이 안넘음
-                            print("운행 시간이 300분이 초과X")
-                            sql_info_update = f"""
-                                  UPDATE logic.group_info
-                                  SET start_time = '{first_start_time}', end_time = '{end_time_s}',
-                                  d_amount = '{d_amount}', c_operating = '{oper_m}' , u_date = NOW() , r_date = NOW()
-                                  WHERE group_id = '{group_id}' 
-                                   """
-                            cursor.execute(sql_info_update)
-                            conn.commit()
-            else:
-                print("으악")
-
+                                cursor.execute(sql_info_update)
+                                conn.commit()
+                            elif time < 300:  # 지금 추가되는 시간이 300이 넘는지
+                                sql_info_update = f"""
+                                                    UPDATE logic.group_info
+                                                    SET start_time = '{first_start_time}', end_time = '{end_time_s}',
+                                                    d_amount = '{d_amount}', c_operating = '{oper_m}' , u_date = NOW() , r_date = NOW()
+                                                    WHERE group_id = '{group_id}'
+                                                     """
+                                cursor.execute(sql_info_update)
+                                conn.commit()
+                            else:  # 운행시간이 300이 안넘음
+                                print("운행 시간이 300분이 초과X")
+                                sql_info_update = f"""
+                                      UPDATE logic.group_info
+                                      SET start_time = '{first_start_time}', end_time = '{end_time_s}',
+                                      d_amount = '{d_amount}', c_operating = '{oper_m}' , u_date = NOW() , r_date = NOW()
+                                      WHERE group_id = '{group_id}' 
+                                       """
+                                cursor.execute(sql_info_update)
+                                conn.commit()
+                else:
+                    print("으악")
     response = {
         "result": "ok"
     }
